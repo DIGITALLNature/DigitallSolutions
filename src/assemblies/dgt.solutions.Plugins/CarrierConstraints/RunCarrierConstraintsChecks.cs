375c2dceb3c6486fb7fb89208ddfee11273dd9af
Micha Oberstein <Moberstein@users.noreply.github.com>
2024-06-30 12:34:16 +0200

    refactor: call custom apis based on name

--- src/assemblies/dgt.solutions.Plugins/CarrierConstraints/RunCarrierConstraintsChecks.cs
+++ src/assemblies/dgt.solutions.Plugins/CarrierConstraints/RunCarrierConstraintsChecks.cs
@@ -43,41 +43,38 @@ namespace dgt.solutions.Plugins.CarrierConstraints
                 };
             else
                 foreach (var constraint in carrier.DgtConstraintMset)
-                    switch (constraint.Value)
+                {
+                    string constraintRequestName = string.Empty;
+                    switch(constraint.Value)
                     {
                         case DgtCarrier.Options.DgtConstraintMset.PreventFlows:
-                            var preventFlowsResponse = ElevatedOrganizationService.Execute(new DgtPreventFlowsRequest
-                            {
-                                Target = workbench.ToEntityReference()
-                            }) as DgtPreventFlowsResponse;
-
-                            yield return new SerializerService().JsonDeserialize<ConstraintCheckLogEntry>(preventFlowsResponse.ConstraintLog_PreventFlows);
+                            constraintRequestName = SdkMessageNames.DgtPreventFlows;
                             break;
                         case DgtCarrier.Options.DgtConstraintMset.PreventItemsWithouthActiveLayer:
-                            var preventItemsWithouthActiveLayerResponse = ElevatedOrganizationService.Execute(new DgtPreventItemsWithoutActiveLayerRequest
-                            {
-                                Target = workbench.ToEntityReference()
-                            }) as DgtPreventItemsWithoutActiveLayerResponse;
-
-                            yield return new SerializerService().JsonDeserialize<ConstraintCheckLogEntry>(preventItemsWithouthActiveLayerResponse.ConstraintLog_PreventItemsWithoutActiveLayer);
+                            constraintRequestName = SdkMessageNames.DgtPreventItemsWithoutActiveLayer;
                             break;
                         case DgtCarrier.Options.DgtConstraintMset.PreventManagedEntitiesWithAllAssets:
-                            var preventManagedEntitiesWithAllAssetsResponse = ElevatedOrganizationService.Execute(new DgtPreventManagedTablesWithAllAssetsRequest
-                            {
-                                Target = workbench.ToEntityReference()
-                            }) as DgtPreventManagedTablesWithAllAssetsResponse;
-
-                            yield return new SerializerService().JsonDeserialize<ConstraintCheckLogEntry>(preventManagedEntitiesWithAllAssetsResponse.ConstraintLog_PreventManagedTablesWithAllAssets);
+                            constraintRequestName = SdkMessageNames.DgtPreventManagedTablesWithAllAssets;
                             break;
                         case DgtCarrier.Options.DgtConstraintMset.PreventPluginAssemblys:
-                            var preventPluginAssemblysResponse = ElevatedOrganizationService.Execute(new DgtPreventPluginAssembliesRequest
-                            {
-                                Target = workbench.ToEntityReference()
-                            }) as DgtPreventPluginAssembliesResponse;
-
-                            yield return new SerializerService().JsonDeserialize<ConstraintCheckLogEntry>(preventPluginAssemblysResponse.ConstraintLog_PreventPluginAssemblies);
+                            constraintRequestName = SdkMessageNames.DgtPreventPluginAssemblies;
                             break;
-                    }
+                    };
+
+                    if (string.IsNullOrEmpty(constraintRequestName))
+                        continue;
+
+                    var constraintCheckResponse = ElevatedOrganizationService.Execute(new OrganizationRequest(constraintRequestName)
+                    {
+                        Parameters = new ParameterCollection
+                        {
+                            { "Target", workbench.ToEntityReference() },
+                        },
+                    });
+
+                    constraintCheckResponse.Results.TryGetValue("Log", out var constraintLog);
+                    yield return new SerializerService().JsonDeserialize<ConstraintCheckLogEntry>(constraintLog as string);
+                }
         }
     }
 }
