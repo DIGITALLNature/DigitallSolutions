2e4a887e38f992c959dfb36d02c7fe0fc02f5930
Micha Oberstein <Moberstein@users.noreply.github.com>
2024-06-30 11:23:17 +0200

    chore: update scripts to sdk style project

--- scripts/push-plugins.ps1
+++ scripts/push-plugins.ps1
@@ -1,5 +1,5 @@
 Push-Location -Path $PSScriptRoot/../
 .\scripts\build-assemblies.ps1
-dgtp push ./src/assemblies/dgt.solutions.Plugins/bin/Release/dgt.solutions.Plugins.dll
+dgtp push ./src/assemblies/dgt.solutions.Plugins/bin/Release/net462/dgt.solutions.Plugins.dll
 
 Pop-Location
